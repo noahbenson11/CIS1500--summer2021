678a242bf79c693bf9764b193180459206dfe804
noahbenson11 <benson.noah11@gmail.com>
2021-05-16 19:39:33 -0400

    Project 0 done

---
+++ Project_0/src/project_0/Project_0.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_0;
+
+/**
+ *
+ * @author Noah Benson
+ */
+public class Project_0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Noah Benson");
+        System.out.println("He/Him");
+        System.out.println("Mechanical Engineering at Michigan State");
+        System.out.println("To gain a credit needed for college of engineering acceptance");
+        System.out.println("I am looking to get a transfer credit and I am excited to learn about coding");
+        System.out.println("I know you said you like video games, I love playing Valorant and Cyberpunk 2077!");
+        
+    }
+    
+}
